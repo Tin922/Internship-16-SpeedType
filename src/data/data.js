c563c92ed4294ee0af0fe7e7a6d2edbafbd57329
Tin922 <tinjuric92@gmail.com>
2024-03-19 22:32:31 +0100

    added more diverse sentences

--- src/data/data.js
+++ src/data/data.js
@@ -1,31 +1,31 @@
 const typingTexts = [
   {
     id: 1,
-    text: "The quick brown fox jumps over the lazy dog. Pack my box with five dozen liquor jugs.",
+    text: "A journey of a thousand miles begins with a single step. The five boxing wizards jump quickly.",
   },
   {
     id: 2,
-    text: "Jackdaws love my big sphinx of quartz. How vexingly quick daft zebras jump!",
+    text: "Don't count your chickens before they hatch. Jinxed wizards pluck ivy from the big quilt.",
   },
   {
     id: 3,
-    text: "Bright vixens jump; dozy fowl quack. The quick brown fox jumps over the lazy dog.",
+    text: "Every cloud has a silver lining. Crazy Fredericka bought many very exquisite opal jewels",
   },
   {
     id: 4,
-    text: "Pack my box with five dozen liquor jugs. Jackdaws love my big sphinx of quartz.",
+    text: "Actions speak louder than words. Sixty zippers were quickly picked from the woven jute bag.",
   },
   {
     id: 5,
-    text: "How vexingly quick daft zebras jump! Bright vixens jump; dozy fowl quack.",
+    text: "Out of sight, out of mind.",
   },
   {
     id: 6,
-    text: "The quick brown fox jumps over the lazy dog. Bright vixens jump; dozy fowl quack.",
+    text: "When the going gets tough, the tough get going. All questions asked by five watched experts amaze the judge.",
   },
   {
     id: 7,
-    text: "Jackdaws love my big sphinx of quartz. Pack my box with five dozen liquor jugs.",
+    text: "You can't judge a book by its cover.",
   },
   {
     id: 8,
@@ -33,7 +33,7 @@ const typingTexts = [
   },
   {
     id: 9,
-    text: "Bright vixens jump; dozy fowl quack. Jackdaws love my big sphinx of quartz.",
+    text: "Bright vixens jump; dozy fowl quack. Jackdaws love my big sphinx of quartz. Jack quietly moved up front and seized the big ball of wax.",
   },
   {
     id: 10,
